9bc89ad435860be087224d52868c1f6c79a43406
Frederik van der Els <fred@xpple.dev>
2025-11-17 18:02:33 +0100

    Replace sout with logger

--- src/main/java/nl/tudelft/smscheckerlib/SmsCheckerLib.java
+++ src/main/java/nl/tudelft/smscheckerlib/SmsCheckerLib.java
@@ -1,9 +1,16 @@
 package nl.tudelft.smscheckerlib;
 
+import nl.tudelft.smscheckerlib.util.LoggerUtil;
 import nl.tudelft.smscheckerlib.util.VersionUtil;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SmsCheckerLib {
+
+    private static final Logger LOGGER = LoggerUtil.getLogger();
+
     public static void main(String... args) {
-        System.out.println(VersionUtil.VERSION);
+        LOGGER.log(Level.INFO, VersionUtil.VERSION);
     }
 }
